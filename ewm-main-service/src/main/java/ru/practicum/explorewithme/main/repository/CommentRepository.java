c9ea2c27b637d8f03ebeba4bdf12d3dfef4de243
Ilya <Em0zzIoN@yandex.ru>
2025-04-30 00:04:27 +0300

    feat(comments): repository

---
+++ ewm-main-service/src/main/java/ru/practicum/explorewithme/main/repository/CommentRepository.java
@@ -0,0 +1,14 @@
+package ru.practicum.explorewithme.main.repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.practicum.explorewithme.main.model.comment.Comment;
+import ru.practicum.explorewithme.main.model.comment.CommentStatus;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+    
+    Page<Comment> findAllByEventIdAndStatus(Long eventId, CommentStatus status, Pageable pageable);
+    
+    Page<Comment> findAllByAuthorId(Long authorId, Pageable pageable);
+}
